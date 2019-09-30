3300b3682edbcc378d47ff5a71abc51a1964e45c
mattshardman <mattshardman@gmail.com>
2019-09-30 11:29:54 +0100

    print bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,8 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
+# It prints Infinity lol
+# Apparently printed if number exceeds 1.797693134862315 * 10 ^ 308
+
 # YOUR CODE HERE
+print(2 ** 65536)
